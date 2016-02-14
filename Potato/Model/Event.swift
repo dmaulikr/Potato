bd91b97329b28d948160eee36c259b2906d84cff
myo3 <myo3@users.noreply.github.com>
2016-02-13 21:20:08 -0500

    Restarted app

---
+++ Potato/Model/Event.swift
@@ -0,0 +1,40 @@
+//
+//  Event.swift
+//  Potato
+//
+//  Events that Potato(s) can carry out.
+//  Includes 9 actions essential for a living potato.
+//  Created by Monica Ong on 2/13/16.
+//  Copyright © 2016 Monica Ong. All rights reserved.
+//
+
+import UIKit
+
+class Event: NSObject {
+    var start: NSDate = NSDate()
+    var end: NSDate = NSDate()
+    var act: Action
+    init (start: NSDate, end: NSDate, act: Action) {
+        self.start = start
+        self.end = end
+        self.act = act
+    }
+    enum Action: String {
+        case Eat = "Eat"
+        case Rise = "Rise" // waking up
+        case Sleep = "Sleep"
+        case Study = "Study"
+        case School = "School"
+        case Work = "Work"
+        case Socialize = "Socialize"
+        case Exercise = "Exercise"
+        case Chill = "Chill"
+        case Date = "Date"
+        case NetflixChill = "NetflixChill"
+        init(act: Action) {
+            self = act
+        }
+    }
+    
+    
+}
---
+++ Potato/Model/Potato.swift
@@ -0,0 +1,98 @@
+//
+//  Potato.swift
+//  Potato
+//
+//  A lively Potato, with a definite meaning in life.
+//  Will help real-life potatoes in their life scheduling
+//
+//  Created by Monica Ong on 2/13/16.
+//  Copyright © 2016 Monica Ong. All rights reserved.
+//
+
+import UIKit
+
+class Potato: NSObject {
+    var schedule = [Week:Event]()
+    var name: Name
+    var mode: Mode
+    //    var growth: Growth
+    
+    enum Name: String {
+        case Elbert = "Elbert"
+        case Donovan = "Donovan"
+        init(n:Name) {
+            self = n
+        }
+    }
+    
+    enum Mode: String {
+        case Bunny = "Bunny"
+        case Tomato = "Tomato"
+        init(mod:Mode) {
+            self = mod
+        }
+    }
+    
+    //    enum Growth: String {
+    //        case Baby = "Baby"
+    //        case Teen = "Teen"
+    //        case Adult = "Adult"
+    //        case Sprout = "Sprout"
+    //        case Plant = "Plant"
+    //        case Tree = "Tree"
+    //        init(growth:Growth) {
+    //            self = growth
+    //        }
+    //    }
+    
+    enum Week: String {
+        case Monday = "Monday"
+        case Tuesday = "Tuesday"
+        case Wednesday = "Wednesday"
+        case Thursday = "Thursday"
+        case Friday = "Friday"
+        case Saturday = "Saturday"
+        case Sunday = "Sunday"
+        init(week:Week){
+            self = week
+        }
+    }
+    
+    init(mode: Mode, name:Name) {
+        //   for index in 1...24 {
+        //     let defaultEvent = Event(time: index, act: .Chill)
+        //   self.schedule.append(defaultEvent)
+        // }
+        self.name = name
+        self.mode = mode
+        //        self.growth = growth
+    }
+    
+    //Adds an Event to the schedule
+    func addEvent(week:Week,event:Event) {
+        schedule[week] = event
+    }
+    
+    //Deletes an Event
+    //Sets the Event action to .Chill
+    func deleteEvent(week:Week,event:Event) {
+        schedule[week]!.act = .Chill
+        
+    }
+    
+    //Changes the mode of the Potato
+    func changeMode(mode:Mode) {
+        self.mode = mode
+    }
+    
+    //Changes the name of the Potato
+    func changeName(name:Name) {
+        self.name = name
+    }
+    
+    //Changes the Growth of the Potato
+    //  func changeGrowth(growth: Growth) {
+    //    self.growth = growth
+    // }
+}
+
